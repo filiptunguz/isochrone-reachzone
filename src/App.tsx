d1dbb0c30165906301a74968d43a03da608c3745
filip.tunguz <filipkralh11@gmail.com>
2025-07-09 09:37:02 +0200

    initialize map polygon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import OpenStreetMap from './components/open-street-map.tsx';
+import { Polygon } from 'react-leaflet';
+import { useState } from 'react';
+import type { LatLngTuple } from 'leaflet';
+import { polygonBounds } from './bounds.ts';
 
 export default function App() {
+	const [bounds, setBounds] = useState<LatLngTuple[]>(polygonBounds.walk['30']);
+
 	return (
 		<div className="p-8">
-			<OpenStreetMap />
+			<OpenStreetMap focusOn={bounds}>
+				<Polygon positions={bounds} color="#244F4B" />
+			</OpenStreetMap>
 		</div>
 	);
 }
--- src/components/open-street-map.tsx
+++ src/components/open-street-map.tsx
@@ -1,7 +1,26 @@
 import { AttributionControl, MapContainer, TileLayer } from 'react-leaflet';
+import type { Map } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import type { LatLngTuple } from 'leaflet';
+import { type ReactNode, useEffect, useState } from 'react';
 
-export default function OpenStreetMap() {
+type OpenStreetMapProps = {
+	focusOn?: LatLngTuple[];
+	children?: ReactNode;
+};
+
+export default function OpenStreetMap({ focusOn, children }: OpenStreetMapProps) {
+	const [mapContainer, setMapContainer] = useState<Map | null>(null);
+
+	useEffect(() => {
+		if (mapContainer && mapContainer && focusOn && focusOn.length > 0) {
+			mapContainer.fitBounds(focusOn, {
+				padding: [25, 25],
+				maxZoom: 15,
+			});
+		}
+	}, [focusOn, mapContainer]);
+
 	return (
 		<div className="rounded-3xl overflow-hidden">
 			<MapContainer
@@ -10,15 +29,17 @@ export default function OpenStreetMap() {
 				scrollWheelZoom={true}
 				className="h-[50vh]"
 				attributionControl={false}
+				ref={setMapContainer}
 			>
 				<TileLayer
 					attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
-					url={`https://maps.geoapify.com/v1/tile/dark-matter-dark-grey/{z}/{x}/{y}.png?apiKey=${import.meta.env.VITE_GEOAPIFY_API_KEY}`}
+					url={`https://api.maptiler.com/maps/winter-v2/{z}/{x}/{y}.png?key=${import.meta.env.VITE_MAP_API_KEY}`}
 					tileSize={512}
 					maxZoom={18}
 					zoomOffset={-1}
 					detectRetina={true}
 				/>
+				{children}
 				<AttributionControl
 					prefix={'<a href="https://leafletjs.com/" target="_blank" rel="noopener">Leaflet</a>'}
 				/>
